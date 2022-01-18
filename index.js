b98e3c5e9bd753f17cec25d7721cb86f2bd7f31f
minirachel <rachel.a.levinson@gmail.com>
2022-01-18 18:04:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const cards = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+writeCards(names);
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
